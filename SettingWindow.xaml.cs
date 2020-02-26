798e475446d674bb57e12d78a19ff5e90f0caaff
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-02-27 08:41:43 +0900

    a

---
+++ SettingWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VisualNovelTool
+{
+    /// <summary>
+    /// SettingWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        public SettingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // 閉じる処理をキャンセルして非表示にする
+            e.Cancel = true;
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
